161acef53a0e3cdacf49abbfe43d72edf20917e0
ustazeynep <zeynep.n.usta@outlook.com>
2023-03-07 21:12:32 +0000

    initial commit

---
+++ src/test/java/com/eurotech/tests/day1_intro/OpenBrowser.java
@@ -0,0 +1,24 @@
+package com.eurotech.tests.day1_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.co.uk/");
+
+        driver.close();
+
+    }
+
+
+
+
+}
