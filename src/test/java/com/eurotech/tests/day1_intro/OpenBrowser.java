7ed9c13db689c70aff1de1bbdddce1498d61e46d
Erhan <erhanilgar@gmail.com>
2023-03-09 19:00:54 +0000

    day2 added

--- src/test/java/com/eurotech/tests/day1_intro/OpenBrowser.java
+++ src/test/java/com/eurotech/tests/day1_intro/OpenBrowser.java
@@ -3,12 +3,16 @@ package com.eurotech.tests.day1_intro;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class OpenBrowser {
 
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
+      //  WebDriverManager.firefoxdriver().setup();
+
+      //  WebDriver driver =new FirefoxDriver();
+
 
         WebDriver driver = new ChromeDriver();
 
@@ -16,9 +20,10 @@ public class OpenBrowser {
 
         driver.close();
 
+
+
+
     }
-
-
 
 
 }
