fd94683bde7ccba91fa82bf20b6e36626a88997c
ustazeynep <zeynep.n.usta@outlook.com>
2023-03-20 21:04:40 +0000

    more testNG assertions added

--- src/test/java/com/eurotech/tests/day7_testNG/testNGAssertions.java
+++ src/test/java/com/eurotech/tests/day7_testNG/testNGAssertions.java
@@ -21,8 +21,21 @@ public class testNGAssertions {
         String expectedText = "zEuro";
         String actualTitle  = "Eurotech";
 //        Assert.assertTrue(actualTitle.startsWith(expectedText));
-        Assert.assertTrue(actualTitle.startsWith(expectedText), "Title does not start with the expected text");
+        Assert.assertTrue(actualTitle.startsWith(expectedText), "Verifying title to start with the expected text");
         System.out.println("test continues");
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertFalse(0>1, "Asserting 0 to be greater than 1");
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertNotEquals("Title", "title");
+       // alternatively
+        boolean result = "Title".equals("title");
+        Assert.assertFalse(result);
     }
 
     @BeforeMethod
